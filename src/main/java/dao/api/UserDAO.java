8c3c5838df72fa82232a3da8017ff9f85a6f0c16
warrior <warrior22@qq.com>
2022-10-02 14:25:41 +0800

    4.1th commit by warrior -- login and register

---
+++ src/main/java/dao/api/UserDAO.java
@@ -0,0 +1,39 @@
+package dao.api;
+
+import pojo.po.User;
+
+/**
+ * @author WARRIOR
+ * @version 1.0
+ */
+public interface UserDAO {
+
+    /**
+     * 根据手机号码（账号）和密码获取用户信息
+     * @param phoneNumber 手机号码（账号）
+     * @param password 密码
+     * @return User or Null
+     */
+    public User getUser(String phoneNumber, String password);
+
+    /**
+     * 创建新用户
+     * @param user
+     * @return 受影响行数
+     */
+    public int addUser(User user);
+
+    /**
+     * 根据手机号码获取用户信息
+     * @param phoneNumber
+     * @return
+     */
+    public User getUserByPhoneNumber(String phoneNumber);
+
+    /**
+     * 根据用户名获取用户信息
+     * @param username
+     * @return
+     */
+    public User getUserByUsername(String username);
+}
